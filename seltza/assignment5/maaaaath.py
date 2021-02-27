8e2c467319ce7e76ded330b7df3b82d229701a17
Alex Carpenter <alex.g.carpenter@gmail.com>
2021-02-27 00:33:13 -0800

    added documentation

--- seltza/assignment5/maaaaath.py
+++ seltza/assignment5/maaaaath.py
@@ -1,3 +1,10 @@
+# Alex Carpenter
+# Colin Reilly School of Brogramming Assignment 5
+# math functions
+# i looked up the PEP guide for docstrings and decided it wasn't worth it
+
+
+
 def add(num1, num2):
     sum = num1 + num2
     print(sum)
