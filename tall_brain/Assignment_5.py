0bd91871de5b3f3fc0ed894ea1affaad99d24120
Harper Scott <harpercscott@gmail.com>
2021-02-27 16:04:47 -0500

    Tried a few things, took me way too long.

--- tall_brain/Assignment_5.py
+++ tall_brain/Assignment_5.py
@@ -1,29 +1,83 @@
-number_1 = 5
-number_2 = 10
+#Attempt 1 Start
+# number_1 = 5
+# number_2 = 10
 
-#Adds predefined constants
-def basic_addition(number_1, number_2):
-    print(number_1 + number_2)
+# #Adds predefined constants
+# def basic_addition(number_1, number_2):
+#     print(number_1 + number_2)
 
-basic_addition(number_1, number_2)
-
-
-#Subtracts predefined constants
-def basic_subtraction(number_1, number_2):
-    print(number_1 - number_2)
-
-basic_subtraction(number_1, number_2)
+# basic_addition(number_1, number_2)
 
 
-#Multiplies predefined contants
-def basic_multiplication(number_1, number_2):
-    print(number_1 * number_2)
+# #Subtracts predefined constants
+# def basic_subtraction(number_1, number_2):
+#     print(number_1 - number_2)
 
-basic_multiplication(number_1, number_2)
+# basic_subtraction(number_1, number_2)
 
 
-#Divides predefined constants
-def basic_division(number_1, number_2):
-    print(number_1 / number_2)
+# #Multiplies predefined contants
+# def basic_multiplication(number_1, number_2):
+#     print(number_1 * number_2)
 
-basic_division(number_1, number_2)
+# basic_multiplication(number_1, number_2)
+
+
+# #Divides predefined constants
+# def basic_division(number_1, number_2):
+#     print(number_1 / number_2)
+
+# basic_division(number_1, number_2)
+#Attempt 1 End
+
+#Attempt 2 Start (Because Attempt 1 works but is inflexible)
+#Using random numbers now might get me docked a letter grade oh god oh fuck
+# import random
+
+# number_1 = random.randrange(1, 10)
+# number_2 = random.randrange(11, 20)
+
+# def add_2(number_1, number_2):
+#     return number_1+number_2
+
+# def subtract_2(number_1, number_2):
+#     return number_1-number_2
+
+# def multiply_2(number_1, number_2):
+#     return number_1*number_2
+
+# def divide_2(number_1, number_2):
+#     return number_1/number_2
+
+# def shit_calculator():
+#     print(add_2(number_1, number_2))
+#     print(subtract_2(number_1, number_2))
+#     print(multiply_2(number_1, number_2))
+#     print(divide_2(number_1, number_2))
+
+# shit_calculator()
+#Attempt 2 End
+
+#Attempt 3
+#Using variables(?) so it works more like a calculator
+#Addition
+def add_3(x, y):
+    return x+y
+
+def subtract_3(x, y):
+    return x-y
+
+def multiply_3(x, y):
+    return x*y
+
+def divide_3(x, y):
+    return x/y
+
+def turd_calculator():
+    print(add_3(1, 2))
+    print(subtract_3(3, 4))
+    print(multiply_3(5, 6))
+    print(divide_3(7, 8))
+
+turd_calculator()
+#Attempt 3 End
