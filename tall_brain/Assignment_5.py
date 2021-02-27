f32557e3f2155ea81071d8afbf70da38ce102ab9
Harper Scott <harpercscott@gmail.com>
2021-02-27 14:25:57 -0500

    This took longer than it should have.

---
+++ tall_brain/Assignment_5.py
@@ -0,0 +1,29 @@
+number_1 = 5
+number_2 = 10
+
+def basic_addition(number_1, number_2):
+    print(number_1 + number_2)
+
+basic_addition(number_1, number_2)
+#Adds predefined constants
+
+
+def basic_subtraction(number_1, number_2):
+    print(number_1 - number_2)
+
+basic_subtraction(number_1, number_2)
+#Subtracts predefined constants
+
+
+def basic_multiplication(number_1, number_2):
+    print(number_1 * number_2)
+
+basic_multiplication(number_1, number_2)
+#Multiplies predefined contants
+
+
+def basic_division(number_1, number_2):
+    print(number_1 / number_2)
+
+basic_division(number_1, number_2)
+#Divides predefined constants
