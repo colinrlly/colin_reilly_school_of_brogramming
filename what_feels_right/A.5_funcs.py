24b331388589903be38e7df3d5f6999288602f66
Chase Moudry <chasemoudry@gmail.com>
2021-02-27 14:37:16 -0700

    A5 Submission

---
+++ what_feels_right/A.5_funcs.py
@@ -0,0 +1,27 @@
+# Requirements:
+# - make a function that adds two numbers together and returns the value
+# - create additional functions for calculating difference, product, and quotient
+
+
+def summ(a, b):
+    return a + b
+
+
+def diff(a, b):
+    return a - b
+
+
+def prod(a, b):
+    return a * b
+
+
+def quot(a, b):
+    if b == 0:
+        return "undefined"
+    return a / b
+
+
+print(summ(3, 2))
+print(diff(3, 2))
+print(prod(3, 2))
+print(quot(3, 2))
