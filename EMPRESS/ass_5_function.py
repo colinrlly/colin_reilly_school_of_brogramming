9f40ac88821e801d892e7945b4c346abcb73aa0b
Nadia Wakabayashi <wakabayashin2013@gmail.com>
2021-02-27 03:30:33 -0800

    Aufgabe Ass 5 HW

---
+++ EMPRESS/ass_5_function.py
@@ -0,0 +1,26 @@
+# Creating Function
+
+# add function adds a and b, returns the result
+def add(a,b):
+    return(a + b)
+
+# mult function multiplies a and b, returns the result
+def mult(a,b):
+    return(a * b)
+
+# sub function subtracts a and b, returns the result
+def sub(a,b):
+    return(a - b)
+
+# div function divides a and b, returns the result
+def div(a,b):
+    return(a / b)
+
+# main function 
+def main():
+    print(add(2,3))
+    print(mult(4,3))
+    print(sub(3,4))
+    print(div(10,5))
+
+main()
