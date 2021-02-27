f9f0c16b3787d143da3d2583fbcb9a934ec94894
Alex Carpenter <alex.g.carpenter@gmail.com>
2021-02-27 00:10:48 -0800

    added the additional 3 functions

---
+++ seltza/assignment5/maaaaath.py
@@ -0,0 +1,20 @@
+def add(num1, num2):
+    sum = num1 + num2
+    print(sum)
+    return sum
+
+def sub(num1, num2):
+    ans = num1-num2
+    print(ans)
+    return ans
+
+def mul(num1, num2):
+    ans = num1*num2
+    print(ans)
+    return ans
+
+def div(num1, num2):
+    ans = num1/num2
+    print(ans)
+    return ans
+
