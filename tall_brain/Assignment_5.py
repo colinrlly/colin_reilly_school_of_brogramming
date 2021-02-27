94faf6670fa2730ce746d8c81fe4a8f2daa7e001
Harper Scott <harpercscott@gmail.com>
2021-02-27 14:29:23 -0500

    This sucks I think

--- tall_brain/Assignment_5.py
+++ tall_brain/Assignment_5.py
@@ -1,29 +1,29 @@
 number_1 = 5
 number_2 = 10
 
+#Adds predefined constants
 def basic_addition(number_1, number_2):
     print(number_1 + number_2)
 
 basic_addition(number_1, number_2)
-#Adds predefined constants
 
 
+#Subtracts predefined constants
 def basic_subtraction(number_1, number_2):
     print(number_1 - number_2)
 
 basic_subtraction(number_1, number_2)
-#Subtracts predefined constants
 
 
+#Multiplies predefined contants
 def basic_multiplication(number_1, number_2):
     print(number_1 * number_2)
 
 basic_multiplication(number_1, number_2)
-#Multiplies predefined contants
 
 
+#Divides predefined constants
 def basic_division(number_1, number_2):
     print(number_1 / number_2)
 
 basic_division(number_1, number_2)
-#Divides predefined constants
