e4bfe8706e23e380a7464480f4e0d9906419fc03
Harper Scott <harpercscott@gmail.com>
2021-02-28 04:03:40 -0500

    i must maintain third place

--- tall_brain/Assignment_7/calculator.py
+++ tall_brain/Assignment_7/calculator.py
@@ -19,24 +19,30 @@ class calculator:
         parsed_prob = prob.split()
         for c in parsed_prob:
             if c.isnumeric():
-                int(c)
+                c_1 = int(c)
+                c_2 = int(c)
+#get variables into proper locations (they get overwritten -> 5 fills both then 4 replaces 5 in both)
             else:
                 if c == "+":
-                    self.add(int(c))
+                    return self.add(c_1, c_2)
                 elif c == "-":
-                    self.sub(int())
+                    return self.sub(c_1, c_2)
                 elif c == "*":
-                    self.mul(int())
+                    return self.mul(c_1, c_2)
                 elif c == "/":
-                    self.div(int())
-
+                    return self.div(c_1, c_2)
+                    
+        
+#c_1 and c_2 do not contain proper information (4 and 5) when self.add are called
+#return statement is scuffed
 
 
 
 calc = calculator()
 
-calc.calculate("5 + 4")
+result = calc.calculate("5 + 4")
 
+print(result)
 
 # print(calc.add(4, 5))
 # print(calc.sub(4, 5))
