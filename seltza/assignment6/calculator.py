fb2188b396471cdaf36b2388d65aa8de353f4aef
Alex Carpenter <alex.g.carpenter@gmail.com>
2021-02-27 17:43:32 -0800

    Added initial implementation as a class

---
+++ seltza/assignment6/calculator.py
@@ -0,0 +1,32 @@
+
+
+class calculator:
+
+    def __init__(self, n1=691, n2=692, ans=-691, op='NA'):
+        self.history = [n1, n2, ans, op]
+
+
+
+    def add(self, n1, n2):
+        ans = n1 + n2
+        history = [n1, n2, ans, '+']
+        print(ans)
+        return ans
+
+    def sub(self, n1, n2):
+        ans = n1-n2
+        history = [n1, n2, ans, '-']
+        return ans
+
+    def mul(self, n1, n2):
+        ans = n1*n2
+        history = [n1, n2, ans, '*']
+        return ans
+
+    def div(self, n1, n2):
+        if n2 != 0:
+            ans = n1/n2
+            history = [n1, n2, ans, '/']
+            return ans
+        raise ValueError('Error, divide by zero. Get out of here with that shit.')
+
