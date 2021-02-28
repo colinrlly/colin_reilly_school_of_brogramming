105926b99a0cfdecebd5281a02ba2ac602fb6308
Harper Scott <harpercscott@gmail.com>
2021-02-28 16:02:49 -0500

    looping was unnecessary myb

--- tall_brain/Assignment_7/calculator.py
+++ tall_brain/Assignment_7/calculator.py
@@ -17,25 +17,25 @@ class calculator:
 
     def calculate(self, prob):
         parsed_prob = prob.split()
-        for c1 in parsed_prob[0]:
-            if c1.isnumeric():
-                c1 = int(c1)
-        for c2 in parsed_prob[2]:
-            if c2.isnumeric():
-                c2 = int(c2)
-        for o in parsed_prob[1]:
-            if o == "+":
-                return self.add(c1, c2)
-            elif o == "-":
-                return self.sub(c1, c2)
-            elif o == "*":
-                return self.mul(c1, c2)
-            elif o == "/":
-                return self.div(c1, c2)
+        c1 = parsed_prob[0]
+        if c1.isnumeric():
+            c1 = int(c1)
+        c2 = parsed_prob[2]
+        if c2.isnumeric():
+            c2 = int(c2)
+        o = parsed_prob[1]
+        if o == "+":
+            return self.add(c1, c2)
+        elif o == "-":
+            return self.sub(c1, c2)
+        elif o == "*":
+            return self.mul(c1, c2)
+        elif o == "/":
+            return self.div(c1, c2)
 
 
 calc = calculator()
 
-result = calc.calculate("2 - 3")
+result = calc.calculate("422 * 232")
 
 print(result)
