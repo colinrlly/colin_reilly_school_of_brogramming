e89285c6e0a7509a8883ff43d6f305d10720f106
Harper Scott <harpercscott@gmail.com>
2021-02-28 17:47:56 -0500

    im so hard

--- tall_brain/Assignment_7/calculator.py
+++ tall_brain/Assignment_7/calculator.py
@@ -29,9 +29,55 @@ class calculator:
         elif o == "/":
             return self.div(c1, c2)
 
+    def AdvCalculate(self, prob):
+        if "+" in prob:
+            adv_parsed_prob = prob.split("+")
+            c1 = adv_parsed_prob[0]
+            c_1 = c1.strip()
+            if c_1.isnumeric():
+                c_1 = int(c_1)
+            c2 = adv_parsed_prob[1]
+            c_2 = c2.strip()
+            if c_2.isnumeric():
+                c_2 = int(c_2)
+            return self.add(c_1, c_2)
+        elif "-" in prob:
+            adv_parsed_prob = prob.split("-")
+            c1 = adv_parsed_prob[0]
+            c_1 = c1.strip()
+            if c_1.isnumeric():
+                c_1 = int(c_1)
+            c2 = adv_parsed_prob[1]
+            c_2 = c2.strip()
+            if c_2.isnumeric():
+                c_2 = int(c_2)
+            return self.sub(c_1, c_2)
+        elif "*" in prob:
+            adv_parsed_prob = prob.split("*")
+            c1 = adv_parsed_prob[0]
+            c_1 = c1.strip()
+            if c_1.isnumeric():
+                c_1 = int(c_1)
+            c2 = adv_parsed_prob[1]
+            c_2 = c2.strip()
+            if c_2.isnumeric():
+                c_2 = int(c_2)
+            return self.mul(c_1, c_2)
+        elif "/" in prob:
+            adv_parsed_prob = prob.split("/")
+            c1 = adv_parsed_prob[0]
+            c_1 = c1.strip()
+            if c_1.isnumeric():
+                c_1 = int(c_1)
+            c2 = adv_parsed_prob[1]
+            c_2 = c2.strip()
+            if c_2.isnumeric():
+                c_2 = int(c_2)
+            return self.div(c_1, c_2)
+
 
 calc = calculator()
 
-result = calc.calculate("422 * 232")
+result = calc.AdvCalculate("10/2")
 
 print(result)
