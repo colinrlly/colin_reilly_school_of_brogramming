944895bee5a49addd47cf46243b1fd906bb1ad39
Harper Scott <harpercscott@gmail.com>
2021-02-28 15:36:36 -0500

    LETS FUCKEN GOOOO

--- tall_brain/Assignment_7/calculator.py
+++ tall_brain/Assignment_7/calculator.py
@@ -17,13 +17,13 @@ class calculator:
 
     def calculate(self, prob):
         parsed_prob = prob.split()
-        for c1 in parsed_prob[0:2]:
+        for c1 in parsed_prob[0]:
             if c1.isnumeric():
                 c1 = int(c1)
-        for c2 in parsed_prob[2:5]:
+        for c2 in parsed_prob[2]:
             if c2.isnumeric():
                 c2 = int(c2)
-        for o in parsed_prob[2:3]:
+        for o in parsed_prob[1]:
             if o == "+":
                 return self.add(c1, c2)
             elif o == "-":
@@ -33,25 +33,9 @@ class calculator:
             elif o == "/":
                 return self.div(c1, c2)
 
-                
-
-# #get variables into proper locations (they get overwritten -> 5 fills both then 4 replaces 5 in both)
-#             else:
-#                 
-                    
-        
-#c_1 and c_2 do not contain proper information (4 and 5) when self.add are called
-#return statement is scuffed
-
-
 
 calc = calculator()
 
-result = calc.calculate("5 + 4")
+result = calc.calculate("2 - 3")
 
 print(result)
-
-# print(calc.add(4, 5))
-# print(calc.sub(4, 5))
-# print(calc.mul(4, 5))
-# print(calc.div(4, 5))
