0623d789b18269490114f47cc1b53dcfdb17f7d5
Alex Carpenter <alex.g.carpenter@gmail.com>
2021-02-27 19:00:54 -0800

    added initial implementation of string parsing added notes on git commands

---
+++ seltza/assignment7/driver.py
@@ -0,0 +1,42 @@
+from calculator import Calculator
+import random
+
+# Alex Carpenter
+# Colin Reilly School of Brogramming Assignment 5
+# Math function demo
+
+c = Calculator(throw=False)
+
+num1 = random.randrange(20)
+num2 = random.randrange(20)
+
+print(str(num1) + ' + ' + str(num2) + " =")
+print(c.add(num1, num2))
+
+num1 = random.randrange(20)
+num2 = random.randrange(20)
+
+print(str(num1) + ' - ' + str(num2) + " =")
+print(c.sub(num1, num2))
+
+num1 = random.randrange(20)
+num2 = random.randrange(20)
+
+print(str(num1) + ' * ' + str(num2) + " =")
+print(c.mul(num1, num2))
+
+num1 = random.randrange(20)
+num2 = random.randrange(1,20)
+
+print(str(num1) + ' / ' + str(num2) + " =")
+print(c.div(num1, num2))
+
+num1 = random.randrange(20)
+num2 = 0
+
+print(str(num1) + ' / ' + str(num2) + " =")
+print(c.div(num1, num2))
+
+print('History:')
+c.hst()
+
