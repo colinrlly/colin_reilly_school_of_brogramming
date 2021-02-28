0ec05117236341e9b840ace7f689641d7ce91c28
Harper Scott <harpercscott@gmail.com>
2021-02-28 15:29:37 -0500

    i think im getting somewhere?

--- tall_brain/Assignment_7/calculator.py
+++ tall_brain/Assignment_7/calculator.py
@@ -17,20 +17,27 @@ class calculator:
 
     def calculate(self, prob):
         parsed_prob = prob.split()
-        for c in parsed_prob:
-            if c.isnumeric():
-                c_1 = int(c)
-                c_2 = int(c)
-#get variables into proper locations (they get overwritten -> 5 fills both then 4 replaces 5 in both)
-            else:
-                if c == "+":
-                    return self.add(c_1, c_2)
-                elif c == "-":
-                    return self.sub(c_1, c_2)
-                elif c == "*":
-                    return self.mul(c_1, c_2)
-                elif c == "/":
-                    return self.div(c_1, c_2)
+        for c1 in parsed_prob[0:2]:
+            if c1.isnumeric():
+                c1 = int(c1)
+        for c2 in parsed_prob[2:5]:
+            if c2.isnumeric():
+                c2 = int(c2)
+        for o in parsed_prob[2:3]:
+            if o == "+":
+                return self.add(c1, c2)
+            elif o == "-":
+                return self.sub(c1, c2)
+            elif o == "*":
+                return self.mul(c1, c2)
+            elif o == "/":
+                return self.div(c1, c2)
+
+                
+
+# #get variables into proper locations (they get overwritten -> 5 fills both then 4 replaces 5 in both)
+#             else:
+#                 
                     
         
 #c_1 and c_2 do not contain proper information (4 and 5) when self.add are called
