b786df6377b43d3903f8ed5c0f9145c3173036b8
virunus <tech+git@virunus.com>
2021-02-28 16:13:32 -0500

    add initial file

---
+++ php-lover/assignment6.py
@@ -0,0 +1,11 @@
+def add(n1: int, n2: int):
+    print(n1 + n2)
+
+def mul(n1: int, n2: int):
+    print(n1 + n2)
+
+def div(n1: int, n2: int):
+    print(n1 / n2)
+
+def sub(n1: int, n2: int):
+    print(n1 - n2)
