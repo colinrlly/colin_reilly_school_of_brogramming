a509736915b82f8ac5a94506f8647c18a3e5724e
Mikhail <chebotarmikhail@gmail.com>
2021-03-02 12:32:34 -0500

    Added empty lines.

--- fakeTA/calculator/calculator/views.py
+++ fakeTA/calculator/calculator/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the caclulator index.")
+    return HttpResponse("Hello, world. You're at the calculator index.")
