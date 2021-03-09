1063bef65801f65c0c61ec70615c5c6e8f8d5237
Harper Scott <harpercscott@gmail.com>
2021-03-09 18:32:50 -0500

    lol thinking too much for something simple

--- tall_brain/tall_brain_online/tall_brain_online/views.py
+++ tall_brain/tall_brain_online/tall_brain_online/views.py
@@ -9,8 +9,6 @@ def index(request):
     return HttpResponse("Hello, brofessor! Please proceed to fugma.")
 
 def calculate(request):
-    print(request.POST)
-    testDict = {
-        'data': request.POST.get('to_calculate', '')
-    }
-    return JsonResponse(testDict)
+    print(json.loads(request.body))
+    return JsonResponse(json.loads(request.body))
+    
