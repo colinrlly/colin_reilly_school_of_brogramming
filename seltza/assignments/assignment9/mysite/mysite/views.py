a103d0bfe645b8dd9cb66a55cf35834a7b2a8225
Alex Carpenter <alex.g.carpenter@gmail.com>
2021-03-10 17:44:42 -0800

    i think this is the assingment done, who knows

--- seltza/assignments/assignment9/mysite/mysite/views.py
+++ seltza/assignments/assignment9/mysite/mysite/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
+
+def calc(obj, expr='4 + 5'):
+    print(expr)
+    ans = obj.calculate(expr)
+    if ans:
+        print(ans)
 
 def index(request):
     return HttpResponse("Hello, world. You're at deez.")
+
+def calc(request):
+    to_calc = request.POST.get('to_calculate')
+    c = Calculator(throw=False)
+    ans = calc(c, to_calc)
+    resp = { 'ans' : str(ans)}
+    return JsonResponse(resp)
