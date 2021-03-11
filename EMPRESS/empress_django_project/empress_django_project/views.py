c79aa33ed955729370950910fc931f9c4381d87f
Nadia Wakabayashi <wakabayashin2013@gmail.com>
2021-03-10 18:02:35 -0800

    what am i dooing

--- EMPRESS/empress_django_project/empress_django_project/views.py
+++ EMPRESS/empress_django_project/empress_django_project/views.py
@@ -1,6 +1,12 @@
+import json
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hewo uwu, world-chan is kawaii desu.")
+
+def calculate(request):
+    if request.method == 'POST':
+        return JsonResponse()
