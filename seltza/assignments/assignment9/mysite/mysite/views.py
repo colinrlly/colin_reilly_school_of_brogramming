a0c3fbc4af31085bd6d229996d1a9bf17bdf6de3
Alex Carpenter <alex.g.carpenter@gmail.com>
2021-03-10 19:21:14 -0800

    i guess this is assignment 10

--- seltza/assignments/assignment9/mysite/mysite/views.py
+++ seltza/assignments/assignment9/mysite/mysite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from .calculator import Calculator
+from django.http import JsonResponse
 
 
 def calc(obj, expr='4 + 5'):
@@ -14,10 +15,12 @@ def index(request):
     return HttpResponse("Hello, world. You're at deez.")
 
 def calculate(request):
-    to_calc = request.POST.get('to_calculate')
-    if to_calc:
-        print(to_calc)
+    bdy = str(request.body)
+    if bdy:
+        parsed = json.loads(request.body)
+        to_calc = parsed['to_calculate']
         c = Calculator(throw=False)
-        ans = calc(c, to_calc)
+        calc(c, to_calc)
+        ans = c.calculate(to_calc)
         resp = { 'ans' : str(ans)}
         return JsonResponse(resp)
