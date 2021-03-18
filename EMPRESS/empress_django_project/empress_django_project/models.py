a798ce855d85c34d13798b790a99a1ba571526b3
Nadia Wakabayashi <wakabayashin2013@gmail.com>
2021-03-17 18:54:41 -0700

    I DID IT

--- EMPRESS/empress_django_project/empress_django_project/models.py
+++ EMPRESS/empress_django_project/empress_django_project/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class CalculateHistory(models.Model):
-    input= models.CharField(max_length=100)
-    result= models.CharField(max_length=100)
+    input = models.CharField(max_length=100)
+    result = models.CharField(max_length=100)
